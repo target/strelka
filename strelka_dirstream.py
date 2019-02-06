246f5bb39bd2c7cbe1d17a59291b921379fa9a96
Zach Hatsis <zhatsis@progrexion.com>
2019-02-05 19:15:52 -0700

    iglob returns a tuple so my last commit busted this.

--- strelka_dirstream.py
+++ strelka_dirstream.py
@@ -112,7 +112,7 @@ class DirWorker(multiprocessing.Process):
                 if self.recursive:
                     iglobpath = f"{self.directory}/**/*"
                 globbed_paths = glob.iglob(pathname=iglobpath, recursive=self.recursive)
-                for (entry) in enumerate(globbed_paths):
+                for _, entry in enumerate(globbed_paths):
                     if os.path.isfile(entry):
                         file_mtime = os.stat(path=entry).st_mtime
                         mtime_delta = current_time - file_mtime
