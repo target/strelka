ff78f55903f889478f6c51dfb18d554d39b5d59b
jshlbrd <liburdi.joshua@gmail.com>
2019-03-08 12:04:40 -0800

    Fix installation of taste.yara
    This commit fixes the installed location of the /etc/strelka/taste/taste.yara file. Prior to this, the server would throw errors indicating the file could not be located.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description="strelka: scanning files at scale with python and zeromq",
     license="Apache 2.0",
     packages=find_packages(),
-    data_files=[("/etc/strelka", ["etc/strelka/strelka.yml", "etc/strelka/pylogging.ini", "etc/strelka/taste/taste.yara", "etc/dirstream/dirstream.yml"])],
+    data_files=[("/etc/strelka", ["etc/strelka/strelka.yml", "etc/strelka/pylogging.ini", "etc/dirstream/dirstream.yml"]), ('/etc/strelka/taste/', ['etc/strelka/taste/taste.yara'])],
     scripts=["strelka.py", "strelka_dirstream.py", "strelka_user_client.py", "generate_curve_certificates.py", "validate_yara.py"],
     zip_safe=False
 )
