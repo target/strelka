c0f70eb4e729adcc8342d27d65a5a0d44f84b90d
jshlbrd <liburdi.joshua@gmail.com>
2019-04-18 11:24:32 -0700

    Delete old layout

--- strelka/scanners/scan_entropy.py
+++
@@ -1,9 +0,0 @@
-import entropy
-
-from strelka import core
-
-
-class ScanEntropy(core.StrelkaScanner):
-    """Calculates entropy of files."""
-    def scan(self, st_file, options):
-        self.metadata['entropy'] = entropy.shannon_entropy(self.data)
