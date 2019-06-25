d472321223c75bd0852f42e5391c1a3a2490d7e4
Josh Liburdi <liburdi.joshua@gmail.com>
2019-06-25 13:07:25 -0700

    ScanElf rewrite (#76)
    * Init ScanElf rewrite
    
    This commit transitions ScanElf from using pyelftools to LIEF (which all three executable scanners will eventually move to). We’re using LIEF 10.0dev due to requiring some incoming functionality.
    
    * Updated CHANGELOG

--- src/python/strelka/scanners/scan_elf.py
+++ src/python/strelka/scanners/scan_elf.py
@@ -1,10 +1,4 @@
-import io
-
-from elftools.common import exceptions
-from elftools.elf import descriptions
-from elftools.elf import dynamic
-from elftools.elf import elffile
-from elftools.elf import sections
+from lief import ELF
 
 from strelka import strelka
 
@@ -12,81 +6,122 @@ from strelka import strelka
 class ScanElf(strelka.Scanner):
     """Collects metadata from ELF files."""
     def scan(self, data, file, options, expire_at):
-        with io.BytesIO(data) as elf_io:
-            try:
-                elf = elffile.ELFFile(elf_io)
+        elf = ELF.parse(raw=data)
 
-                self.event.setdefault('header', {})
-                for (key, value) in elf.header.items():
-                    if key == 'e_flags':
-                        self.event['header']['flags'] = value
-                    elif key == 'e_shnum':
-                        self.event['header']['section_headers'] = value
-                    elif key == 'e_phnum':
-                        self.event['header']['program_headers'] = value
-                    elif key == 'e_version':
-                        self.event['header']['file_version'] = descriptions.describe_e_version_numeric(value)
-                    elif key == 'e_machine':
-                        self.event['header']['machine'] = descriptions.describe_e_machine(value)
-                    elif key == 'e_type':
-                        self.event['header']['file_type'] = descriptions.describe_e_type(value)
-                    elif key == 'e_ident':
-                        for x, y in value.items():
-                            if x == 'EI_DATA':
-                                self.event['header']['data'] = descriptions.describe_ei_data(y)
-                            elif x == 'EI_OSABI':
-                                self.event['header']['os_abi'] = descriptions.describe_ei_osabi(y)
-                            elif x == 'EI_VERSION':
-                                self.event['header']['header_version'] = descriptions.describe_ei_version(y)
-                            elif x == 'EI_CLASS':
-                                self.event['header']['class'] = descriptions.describe_ei_class(y)
-                            elif x == 'EI_ABIVERSION':
-                                self.event['header']['abi_version'] = y
+        self.event['total'] = {
+            'relocations': len(elf.relocations),
+            'sections': elf.header.numberof_sections,
+            'segments': elf.header.numberof_segments,
+        }
 
-                try:
-                    self.event.setdefault('shared_libraries', [])
-                    self.event.setdefault('imports', [])
-                    self.event.setdefault('exports', [])
-                    for section in elf.iter_sections():
-                        if not section.is_null():
-                            if isinstance(section, dynamic.DynamicSection):
-                                for tag in section.iter_tags():
-                                    if tag.entry.d_tag == 'DT_NEEDED':
-                                        if tag.needed not in self.event['shared_libraries']:
-                                            self.event['shared_libraries'].append(tag.needed)
+        self.event['entrypoint'] = elf.entrypoint
+        self.event['imagebase'] = elf.imagebase
+        self.event['name'] = elf.name
+        self.event['nx'] = elf.has_nx
+        self.event['pie'] = elf.is_pie
 
-                            # Attempt to organize symbols into imports and exports
-                            # This is the most comprehensive explanation I've seen for organizing these symbols: http://www.m4b.io/elf/export/binary/analysis/2015/05/25/what-is-an-elf-export.html
-                            if isinstance(section, sections.SymbolTableSection):
-                                for symbol in section.iter_symbols():
-                                    if descriptions.describe_symbol_type(symbol['st_info']['type']) in ['FUNC', 'OBJECT']:
-                                        if descriptions.describe_symbol_bind(symbol['st_info']['bind']) in ['GLOBAL', 'WEAK']:
-                                            if descriptions.describe_symbol_shndx(symbol['st_shndx']) == 'UND':
-                                                if symbol.name not in self.event['imports']:
-                                                    self.event['imports'].append(symbol.name)
-                                            else:
-                                                if symbol.name not in self.event['exports']:
-                                                    self.event['exports'].append(symbol.name)
+        self.event['header'] = {
+            'endianness': str(elf.header.identity_data).split('.')[1],
+            'file': {
+                'type': str(elf.header.file_type).split('.')[1],
+                'version': str(elf.header.object_file_version).split('.')[1],
+            },
+            'flags': {
+                'arm': [str(f).split('.')[1] for f in elf.header.arm_flags_list],
+                'hexagon': [str(f).split('.')[1] for f in elf.header.hexagon_flags_list],
+                'mips': [str(f).split('.')[1] for f in elf.header.mips_flags_list],
+                'ppc64': [str(f).split('.')[1] for f in elf.header.ppc64_flags_list],
+                'processor': elf.header.processor_flag,
+            },
+            'identity': {
+                'class': str(elf.header.identity_class).split('.')[1],
+                'data': str(elf.header.identity_data).split('.')[1],
+                'os_abi': str(elf.header.identity_os_abi).split('.')[1],
+                'version': str(elf.header.identity_version).split('.')[1],
+            },
+            'machine': str(elf.header.machine_type).split('.')[1],
+            'size': elf.header.header_size,
+        }
 
-                except OverflowError:
-                    self.flags.append('overflow_error')
+        if elf.has_interpreter:
+            self.event['interpreter'] = elf.interpreter
 
-                self.event.setdefault('segments', [])
-                segment_cache = {}
-                for segment in elf.iter_segments():
-                    for section in elf.iter_sections():
-                        if not section.is_null() and segment.section_in_segment(section):
-                            key = segment.header['p_type']
-                            if key not in self.event['segments']:
-                                self.event['segments'].append(key)
-                                segment_cache.setdefault(key, [])
-                            segment_cache[key].append(section.name)
+        self.event.setdefault('relocations', [])
+        self.event['relocations'] = []
+        for relo in elf.relocations:
+            row = {
+                'address': relo.address,
+                'info': relo.info,
+                'purpose': str(relo.purpose).split('.')[1],
+                'size': relo.size,
+            }
 
-                self.event.setdefault('segment_sections', [])
-                for (key, value) in segment_cache.items():
-                    section_dict = {'segment': key, 'sections': value}
-                    if section_dict not in self.event['segment_sections']:
-                        self.event['segment_sections'].append(section_dict)
+            if relo.has_section:
+                row['symbol']: relo.section.name
 
-            except exceptions.ELFParseError:
-                self.flags.append('elf_parse_error')
+            if relo.has_symbol:
+                row['symbol']: relo.symbol.name
+
+            if elf.header.machine_type == ELF.ARCH.x86_64:
+                row['type'] = str(ELF.RELOCATION_X86_64(relo.type)).split('.')[1]
+            elif elf.header.machine_type == ELF.ARCH.i386:
+                row['type'] = str(ELF.RELOCATION_i386(relo.type)).split('.')[1]
+            elif elf.header.machine_type == ELF.ARCH.ARM:
+                row['type'] = str(ELF.RELOCATION_ARM(relo.type)).split('.')[1]
+            elif elf.header.machine_type == ELF.ARCH.AARCH64:
+                row['type'] = str(ELF.RELOCATION_AARCH64(relo.type)).split('.')[1]
+            else:
+                row['type'] = str(relo.type)
+
+            self.event['relocations'].append(row)
+
+        self.event['sections'] = []
+        for sec in elf.sections:
+            self.event['sections'].append({
+                'entropy': sec.entropy,
+                'flags': [str(f).split('.')[1] for f in sec.flags_list],
+                'name': sec.name,
+                'offset': sec.offset,
+                'size': sec.size,
+                'type': str(sec.type).split('.')[1],
+                'segments': [str(seg.type).split('.')[1] for seg in sec.segments],
+            })
+
+        self.event['segments'] = []
+        for seg in elf.segments:
+            self.event['segments'].append({
+                'alignment': seg.alignment,
+                'file_offset': seg.file_offset,
+                'physical': {
+                    'address': seg.physical_address,
+                    'size': seg.physical_size,
+                },
+                'sections': [str(sec.name).split('.')[1] for sec in seg.sections],
+                'type': str(seg.type).split('.')[1],
+                'virtual': {
+                    'address': seg.virtual_address,
+                    'size': seg.virtual_size,
+                },
+            })
+
+        self.event['symbols'] = {
+            'exported': [sym.name for sym in elf.exported_symbols],
+            'imported': [sym.name for sym in elf.imported_symbols],
+            'table': [],
+        }
+
+        for sym in elf.symbols:
+            self.event['symbols']['table'].append({
+                'binding': str(sym.binding).rsplit('.')[1],
+                'information': sym.information,
+                'function': sym.is_function,
+                'name': sym.name,
+                'section_index': str(ELF.SYMBOL_SECTION_INDEX(sym.shndx)).rsplit('.')[1],
+                'size': sym.size,
+                'static': sym.is_static,
+                'symbol_version': str(sym.symbol_version),
+                'type': str(sym.type).rsplit('.')[1],
+                'value': sym.value,
+                'variable': sym.is_variable,
+                'visibility': str(sym.visibility).rsplit('.')[1],
+            })
