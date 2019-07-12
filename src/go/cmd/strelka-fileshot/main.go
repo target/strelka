2b0f87601ffd4368de006c06f2259a269233613e
jshlbrd <liburdi.joshua@gmail.com>
2019-07-12 11:05:11 -0700

    Add server dial timeout option
    This commit makes the server dial timeout a configurable option for both Go clients.

--- src/go/cmd/strelka-fileshot/main.go
+++ src/go/cmd/strelka-fileshot/main.go
@@ -9,7 +9,6 @@ import (
         "path/filepath"
         "runtime/pprof"
         "sync"
-        "time"
 
         "google.golang.org/grpc"
         "gopkg.in/yaml.v2"
@@ -59,7 +58,7 @@ func main() {
 
         serv := conf.Conn.Server
         auth := rpc.SetAuth(conf.Conn.Cert)
-        ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+        ctx, cancel := context.WithTimeout(context.Background(), conf.Conn.Timeout.Dial)
         defer cancel()
         conn, err := grpc.DialContext(ctx, serv, auth, grpc.WithBlock())
         if err != nil {
@@ -146,7 +145,7 @@ func main() {
                         go func(){
                                 rpc.ScanFile(
                                         frontend,
-                                        conf.Conn.Timeout,
+                                        conf.Conn.Timeout.File,
                                         req,
                                         responses,
                                 )
--- src/go/cmd/strelka-filestream/main.go
+++ src/go/cmd/strelka-filestream/main.go
@@ -59,7 +59,7 @@ func main() {
 
         serv := conf.Conn.Server
         auth := rpc.SetAuth(conf.Conn.Cert)
-        ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+        ctx, cancel := context.WithTimeout(context.Background(), conf.Conn.Timeout.Dial)
         defer cancel()
         conn, err := grpc.DialContext(ctx, serv, auth, grpc.WithBlock())
         if err != nil {
@@ -148,7 +148,7 @@ func main() {
                         go func(){
                                 rpc.ScanFile(
                                         frontend,
-                                        conf.Conn.Timeout,
+                                        conf.Conn.Timeout.File,
                                         req,
                                         responses,
                                 )
@@ -204,7 +204,7 @@ func main() {
                                 go func(){
                                         rpc.ScanFile(
                                                 frontend,
-                                                conf.Conn.Timeout,
+                                                conf.Conn.Timeout.File,
                                                 req,
                                                 responses,
                                         )
