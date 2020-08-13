3f6fb0ea8978c105178ac302d21e018343d74dca
Paul Hutelmyer <Paul.Hutelmyer@target.com>
2020-08-13 08:40:22 -0400

    Updating error handling

--- src/python/strelka/scanners/scan_capa.py
+++ src/python/strelka/scanners/scan_capa.py
@@ -1,4 +1,5 @@
 import re
+import os
 import json
 import subprocess
 import tempfile
@@ -20,47 +21,56 @@ class ScanCapa(strelka.Scanner):
         tmp_directory = options.get('tmp_directory', '/tmp/')
         location = options.get('location', '/etc/capa/')
 
-        try:
-            with tempfile.NamedTemporaryFile(dir=tmp_directory) as tmp_data:
-                tmp_data.write(data)
-                tmp_data.flush()
-
-                try:
-                    (stdout, stderr) = subprocess.Popen(
-                        ['/tmp/capa-linux', tmp_data.name, '-r', location, '-j'],
-                        stdout=subprocess.PIPE,
-                        stderr=subprocess.DEVNULL
-                    ).communicate()
-                except:
-                    self.flags.append('processing_error')
-
-                if stdout:
-                    # Observed extraneous data in stdout requiring string trimming. Parse out JSON response.
-                    # This can be fixed when CAPA is aviailable as a Python 3 library.
-                    try:
-                        stdout = stdout[stdout.find(b'{'):]
-                        stdout = stdout[:stdout.rfind(b'}')]
-                        stdout += b'}'
-                        capa_json = json.loads(stdout)
-                    except:
-                        self.flags.append('parsing_error')
-
-                    # Sets are used to remove duplicative values
-                    self.event['matches'] = set()
-                    self.event['attack'] = set()
+        # Only run if rules file exists, otherwise return no rules error
+        if len(os.listdir(location)) != 0:
+            try:
+                with tempfile.NamedTemporaryFile(dir=tmp_directory) as tmp_data:
+                    tmp_data.write(data)
+                    tmp_data.flush()
 
                     try:
-                        for k, v in capa_json['rules'].items():
-                            self.event['matches'].add(k)
-                            if 'att&ck' in v['meta']:
-                                result = re.search(r'^([^:]+)::([^\[)]+)\s\[([^\]]+)\]', v['meta']['att&ck'][0])
-                                self.event['attack'].add(result.group(2))
-                        # For consistency, convert sets to list
-                        self.event['matches'] = list(self.event['matches'])
-                        self.event['attack'] = list(self.event['attack'])
+                        (stdout, stderr) = subprocess.Popen(
+                            ['/tmp/capa-linux', tmp_data.name, '-r', location, '-j'],
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.DEVNULL
+                        ).communicate()
                     except:
-                        self.flags.append('collection_error')
-        except:
-            self.flags.append('scan_error')
+                        self.flags.append('error_processing')
+                        return
+
+                    if stdout:
+                        # Observed extraneous data in stdout requiring string trimming. Parse out JSON response.
+                        # This can be fixed when CAPA is aviailable as a Python 3 library.
+                        try:
+                            stdout = stdout[stdout.find(b'{'):]
+                            stdout = stdout[:stdout.rfind(b'}')]
+                            stdout += b'}'
+                            capa_json = json.loads(stdout)
+                        except:
+                            self.flags.append('error_parsing')
+                            return
+
+                        try:
+                            # Sets are used to remove duplicative values
+                            self.event['matches'] = set()
+                            self.event['mitre_techniques'] = set()
+                            self.event['mitre_ids'] = set()
+
+                            for k, v in capa_json['rules'].items():
+                                self.event['matches'].add(k)
+                                if 'att&ck' in v['meta']:
+                                    result = re.search(r'^([^:]+)::([^\[)]+)\s\[([^\]]+)\]', v['meta']['att&ck'][0])
+                                    self.event['mitre_techniques'].add(result.group(2))
+                                    self.event['mitre_ids'].add(result.group(3))
+                            # For consistency, convert sets to list
+                            self.event['matches'] = list(self.event['matches'])
+                            self.event['mitre_techniques'] = list(self.event['mitre_techniques'])
+                            self.event['mitre_ids'] = list(self.event['mitre_ids'])
+                        except:
+                            self.flags.append('error_collection')
+            except:
+                self.flags.append('error_execution')
+        else:
+            self.flags.append('error_norules')
 
 
