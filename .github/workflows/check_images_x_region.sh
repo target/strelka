e494fe69869d0de9d24f16f43b96f7ad356f2313
Cameron Dunn <88673576+cameron-dunn-sublime@users.noreply.github.com>
2021-12-23 11:17:47 -0800

    Updated version of check_images_x_region & Tags Fix (#10)
    * Use updated version of check_images_x_region
    
    And use tagged version of configure-aws-credentials
    
    * Remove tag from push --all-tags
    
    This used to work, but now gives an error. I guess it is funny
    to say "all-tags" and then specify a tag.

--- .github/workflows/check_images_x_region.sh
+++ .github/workflows/check_images_x_region.sh
@@ -4,6 +4,11 @@ set -e
 
 tag=$1
 
+timeout_seconds=120
+
+# Use a variable because return 1 will exit the script with `set -e`, but we want to retry.
+checkImageResult="false"
+
 function checkImages() {
   repo=$1
   primary_sha=$(aws ecr describe-images --region us-east-1 --repository-name $repo --image-ids imageTag=$tag | jq -r '.imageDetails[0].imageDigest')
@@ -13,12 +18,40 @@ function checkImages() {
 
     if [[ "$sha" != "$primary_sha" ]]; then
       echo "Incomplete ECR propagation for repo $repo (tag $tag) in $region. Expected $primary_sha but found $sha."
-      exit 1
+      return 0
     fi
+
+    echo "ECR propagation for repo $repo (tag $tag) in $region is complete. Found $sha"
   done
+
+  checkImageResult="true"
+
+  return 0
 }
 
-checkImages strelka-frontend
-checkImages strelka-backend
-checkImages strelka-manager
-checkImages strelka-mmrpc
+function checkImagesWithTimeout() {
+  repo=$1
+
+  end=$((SECONDS+$timeout_seconds))
+
+  while [ $SECONDS -lt $end ]; do
+      checkImageResult="false"
+      checkImages $repo
+
+      if [ "$checkImageResult" = "true" ]; then
+          return 0
+      fi
+      echo "Replication has not finished for $repo, sleeping."
+      sleep 5
+  done
+
+  echo "Replication did not finish for $repo after waiting $timeout_seconds"
+
+  # Just exit instead of checking status codes below (although it doesn't matter with `set -e`
+  exit 1
+}
+
+checkImagesWithTimeout strelka-frontend
+checkImagesWithTimeout strelka-backend
+checkImagesWithTimeout strelka-manager
+checkImagesWithTimeout strelka-mmrpc
