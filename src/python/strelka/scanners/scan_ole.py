0135d846016102de8e4dbba1fc4a5d65af74c814
Cameron Dunn <88673576+cameron-dunn-sublime@users.noreply.github.com>
2022-01-03 15:44:37 -0800

    Suppress Unnecessary Logs & Fix an Undefined Reference (#12)
    * Turn off mupdf errrors & upgrade version

--- src/python/strelka/scanners/scan_ole.py
+++ src/python/strelka/scanners/scan_ole.py
@@ -13,55 +13,52 @@ class ScanOle(strelka.Scanner):
         self.event['total'] = {'streams': 0, 'extracted': 0}
 
         try:
-            ole = olefile.OleFileIO(data)
-            ole_streams = ole.listdir(streams=True)
-            self.event['total']['streams'] = len(ole_streams)
-            for stream in ole_streams:
-                file = ole.openstream(stream)
-                extract_data = file.read()
-                extract_name = f'{"_".join(stream)}'
-                extract_name = re.sub(r'[\x00-\x1F]', '', extract_name)
-                if extract_name.endswith('Ole10Native'):
-                    native_stream = oletools.oleobj.OleNativeStream(
-                        bindata=extract_data,
-                    )
-                    if native_stream.filename:
-                        extract_name = extract_name + f'_{str(native_stream.filename)}'
+            with olefile.OleFileIO(data) as ole:
+                ole_streams = ole.listdir(streams=True)
+                self.event['total']['streams'] = len(ole_streams)
+                for stream in ole_streams:
+                    file = ole.openstream(stream)
+                    extract_data = file.read()
+                    extract_name = f'{"_".join(stream)}'
+                    extract_name = re.sub(r'[\x00-\x1F]', '', extract_name)
+                    if extract_name.endswith('Ole10Native'):
+                        native_stream = oletools.oleobj.OleNativeStream(
+                            bindata=extract_data,
+                        )
+                        if native_stream.filename:
+                            extract_name = extract_name + f'_{str(native_stream.filename)}'
+                        else:
+                            extract_name = extract_name + '_native_data'
+
+                        extract_file = strelka.File(
+                            name=extract_name,
+                            source=self.name,
+                        )
+
+                        for c in strelka.chunk_string(native_stream.data):
+                            self.upload_to_coordinator(
+                                extract_file.pointer,
+                                c,
+                                expire_at,
+                            )
+
                     else:
-                        extract_name = extract_name + '_native_data'
-
-                    extract_file = strelka.File(
-                        name=extract_name,
-                        source=self.name,
-                    )
-
-                    for c in strelka.chunk_string(native_stream.data):
-                        self.upload_to_coordinator(
-                            extract_file.pointer,
-                            c,
-                            expire_at,
+                        extract_file = strelka.File(
+                            name=extract_name,
+                            source=self.name,
                         )
 
-                else:
-                    extract_file = strelka.File(
-                        name=extract_name,
-                        source=self.name,
-                    )
+                        for c in strelka.chunk_string(extract_data):
+                            self.upload_to_coordinator(
+                                extract_file.pointer,
+                                c,
+                                expire_at,
+                            )
 
-                    for c in strelka.chunk_string(extract_data):
-                        self.upload_to_coordinator(
-                            extract_file.pointer,
-                            c,
-                            expire_at,
-                        )
-
-                self.files.append(extract_file)
-                self.event['total']['extracted'] += 1
+                    self.files.append(extract_file)
+                    self.event['total']['extracted'] += 1
 
         except OSError:
             type, value, traceback = sys.exc_info()
             self.flags.append('os_error')
             self.flags.append(f' {type} {value} {traceback}')
-        finally:
-            if ole is not None:
-                ole.close()
