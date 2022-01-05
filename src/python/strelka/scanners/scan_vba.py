d535c01584f9f4473b2c3fd1b0bd422cb6512c3f
Cameron Dunn <88673576+cameron-dunn-sublime@users.noreply.github.com>
2022-01-05 11:04:32 -0800

    Revert earlier change to use with (#16)
    It turns out this requires a __enter__ method which vba parser
    doesn't implement (and my IDE doesn't know about...).
    
    I thought I'd tested this but I hadn't. Luckily I did before tagging
    latest.

--- src/python/strelka/scanners/scan_vba.py
+++ src/python/strelka/scanners/scan_vba.py
@@ -16,49 +16,52 @@ class ScanVba(strelka.Scanner):
         self.event['total'] = {'files': 0, 'extracted': 0}
 
         try:
-            with olevba.VBA_Parser(filename=file.name, data=data) as vba:
-                if vba.detect_vba_macros():
-                    extract_macros = list(vba.extract_macros())
-                    self.event['total']['files'] = len(extract_macros)
-                    for (filename, stream_path, vba_filename, vba_code) in extract_macros:
-                        extract_file = strelka.File(
-                            name=f'{vba_filename}',
-                            source=self.name,
+            vba = olevba.VBA_Parser(filename=file.name, data=data)
+            if vba.detect_vba_macros():
+                extract_macros = list(vba.extract_macros())
+                self.event['total']['files'] = len(extract_macros)
+                for (filename, stream_path, vba_filename, vba_code) in extract_macros:
+                    extract_file = strelka.File(
+                        name=f'{vba_filename}',
+                        source=self.name,
+                    )
+
+                    for c in strelka.chunk_string(vba_code):
+                        self.upload_to_coordinator(
+                            extract_file.pointer,
+                            c,
+                            expire_at,
                         )
 
-                        for c in strelka.chunk_string(vba_code):
-                            self.upload_to_coordinator(
-                                extract_file.pointer,
-                                c,
-                                expire_at,
-                            )
+                    self.files.append(extract_file)
+                    self.event['total']['extracted'] += 1
 
-                        self.files.append(extract_file)
-                        self.event['total']['extracted'] += 1
-
-                    if analyze_macros:
-                        self.event.setdefault('auto_exec', [])
-                        self.event.setdefault('base64', [])
-                        self.event.setdefault('dridex', [])
-                        self.event.setdefault('hex', [])
-                        self.event.setdefault('ioc', [])
-                        self.event.setdefault('suspicious', [])
-                        macros = vba.analyze_macros()
-                        for (macro_type, keyword, description) in macros:
-                            if macro_type == 'AutoExec':
-                                self.event['auto_exec'].append(keyword)
-                            elif macro_type == 'Base64 String':
-                                self.event['base64'].append(keyword)
-                            elif macro_type == 'Dridex String':
-                                self.event['dridex'].append(keyword)
-                            elif macro_type == 'Hex String':
-                                self.event['hex'].append(keyword)
-                            elif macro_type == 'IOC':
-                                self.event['ioc'].append(keyword)
-                            elif macro_type == 'Suspicious':
-                                self.event['suspicious'].append(keyword)
-                            elif macro_type == 'VBA obfuscated Strings':
-                                self.event['vba_obfuscated'].append(keyword)
+                if analyze_macros:
+                    self.event.setdefault('auto_exec', [])
+                    self.event.setdefault('base64', [])
+                    self.event.setdefault('dridex', [])
+                    self.event.setdefault('hex', [])
+                    self.event.setdefault('ioc', [])
+                    self.event.setdefault('suspicious', [])
+                    macros = vba.analyze_macros()
+                    for (macro_type, keyword, description) in macros:
+                        if macro_type == 'AutoExec':
+                            self.event['auto_exec'].append(keyword)
+                        elif macro_type == 'Base64 String':
+                            self.event['base64'].append(keyword)
+                        elif macro_type == 'Dridex String':
+                            self.event['dridex'].append(keyword)
+                        elif macro_type == 'Hex String':
+                            self.event['hex'].append(keyword)
+                        elif macro_type == 'IOC':
+                            self.event['ioc'].append(keyword)
+                        elif macro_type == 'Suspicious':
+                            self.event['suspicious'].append(keyword)
+                        elif macro_type == 'VBA obfuscated Strings':
+                            self.event['vba_obfuscated'].append(keyword)
 
         except olevba.FileOpenError:
             self.flags.append('file_open_error')
+        finally:
+            if vba is not None:
+                vba.close()
