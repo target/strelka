d498c652caace7add88638b3981f81739cf4a16f
Paul Hutelmyer <paul.hutelmyer@gmail.com>
2022-12-07 07:01:10 -0500

    Reformatting

--- src/python/strelka/tests/test_scan_gif.py
+++ src/python/strelka/tests/test_scan_gif.py
@@ -4,18 +4,27 @@ from strelka.scanners.scan_gif import ScanGif
 
 
 def test_scan_gif(mocker):
-    """Attach trailer index"""
+    # Attach trailer index
 
     scanner = ScanGif(
-        {"name": "ScanGif", "key": "scan_gif", "limits": {"scanner": 10}},
+        {
+            "name": "ScanGif",
+            "key": "scan_gif",
+            "limits": {"scanner": 10}
+        },
         "test_coordinate",
     )
 
     mocker.patch.object(ScanGif, "upload_to_coordinator", return_value=None)
     scanner.scan_wrapper(
         Path(Path(__file__).parent / "fixtures/test.gif").read_bytes(),
-        {"uid": "12345", "name": "somename"},
-        {"scanner_timeout": 5},
+        {
+            "uid": "12345",
+            "name": "somename"
+        },
+        {
+            "scanner_timeout": 5
+        },
         datetime.date.today(),
     )
     assert scanner.event.get("trailer_index") == 3806
--- src/python/strelka/tests/test_scan_url.py
+++ src/python/strelka/tests/test_scan_url.py
@@ -2,10 +2,15 @@ import pytest
 import datetime
 from strelka.scanners.scan_url import ScanUrl
 
+
 scanner = ScanUrl(
-        {"name": "ScanUrl", "key": "scan_url", "limits": {"scanner": 10}},
-        "test_coordinate",
-    )
+    {
+        "name": "ScanUrl",
+        "key": "scan_url",
+        "limits": {"scanner": 10}
+    },
+    "test_coordinate",
+)
 
 tests = [
     (b"some othervalue foo", []),
@@ -13,16 +18,20 @@ tests = [
     (b"foo http://foobar.test.com bar", [b"http://foobar.test.com"]),
     (b"http://\n", []),
     (b"noschema.foo\n", [b"noschema.foo"]),
-    ]
+]
+
 
 @pytest.mark.parametrize("data,expected", tests)
 def test_scan_simple_url(data, expected):
-    """Extract URLs from payloads"""
+    # Extract URLs from payloads
 
     scanner.scan_wrapper(
         data,
         "somefile.foo",
-        {"length": 4, "scanner_timeout": 5},
+        {
+            "length": 4,
+            "scanner_timeout": 5
+        },
         datetime.date.today(),
     )
     assert scanner.event.get("urls") == expected
