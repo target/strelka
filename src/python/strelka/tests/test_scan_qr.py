4bf357264e8f64bb7a96bf77e2da2ed892726b77
Ryan O'Horo <10855297+ryanohoro@users.noreply.github.com>
2022-12-19 00:49:25 -0600

    Test for ScanQr

---
+++ src/python/strelka/tests/test_scan_qr.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+from unittest import TestCase, mock
+
+from strelka.scanners.scan_qr import ScanQr as ScanUnderTest
+from strelka.tests import run_test_scan
+
+
+def test_scan_qr(mocker):
+    """
+    Pass: Sample event matches output of scanner.
+    Failure: Unable to load file or sample event fails to match.
+    """
+
+    test_scan_event = {
+        "elapsed": mock.ANY,
+        "flags": [],
+        "data": "https://www.example.com/",
+        "type": "url",
+    }
+
+    scanner_event = run_test_scan(
+        mocker=mocker,
+        scan_class=ScanUnderTest,
+        fixture_path=Path(__file__).parent / "fixtures/test_qr.png",
+    )
+
+    TestCase.maxDiff = None
+    TestCase().assertDictEqual(test_scan_event, scanner_event)
