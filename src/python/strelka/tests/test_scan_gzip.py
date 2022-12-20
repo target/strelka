3670b4eabb4810f11026a1ecec4e33921cc12c35
Ryan O'Horo <10855297+ryanohoro@users.noreply.github.com>
2022-12-19 18:49:29 -0600

    Test for ScanGzip

---
+++ src/python/strelka/tests/test_scan_gzip.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+from unittest import TestCase, mock
+
+from strelka.scanners.scan_gzip import ScanGzip as ScanUnderTest
+from strelka.tests import run_test_scan
+
+
+def test_scan_gzip(mocker):
+    """
+    Pass: Sample event matches output of scanner.
+    Failure: Unable to load file or sample event fails to match.
+    """
+
+    test_scan_event = {"elapsed": mock.ANY, "flags": [], "size": 4015}
+
+    scanner_event = run_test_scan(
+        mocker=mocker,
+        scan_class=ScanUnderTest,
+        fixture_path=Path(__file__).parent / "fixtures/test.gz",
+    )
+
+    TestCase.maxDiff = None
+    TestCase().assertDictEqual(test_scan_event, scanner_event)
