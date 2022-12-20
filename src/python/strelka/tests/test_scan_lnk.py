950bbc0f9e2402170bc4ce67bcaaa8e41514589f
Ryan O'Horo <10855297+ryanohoro@users.noreply.github.com>
2022-12-19 21:33:53 -0600

    Refactor test_scan_lnk.py with shorthand test

--- src/python/strelka/tests/test_scan_lnk.py
+++ src/python/strelka/tests/test_scan_lnk.py
@@ -1,20 +1,17 @@
-import datetime
 from pathlib import Path
 from unittest import TestCase, mock
 
-from strelka.scanners.scan_lnk import ScanLNK
+from strelka.scanners.scan_lnk import ScanLNK as ScanUnderTest
+from strelka.tests import run_test_scan
 
 
 def test_scan_lnk(mocker):
     """
-    This tests the ScanLNK scanner.
-    It attempts to validate several given LNK metadata values.
-
-    Pass: Metadata values from file match specified values.
-    Failure: Unable to load file or metadata values do not match specified values.
+    Pass: Sample event matches output of scanner.
+    Failure: Unable to load file or sample event fails to match.
     """
 
-    test_scan_lnk_event = {
+    test_scan_event = {
         "elapsed": mock.ANY,
         "flags": [],
         "drive_type": "DRIVE_FIXED",
@@ -29,18 +26,11 @@ def test_scan_lnk(mocker):
         "mac": "38fc989e18fc",
     }
 
-    scanner = ScanLNK(
-        {"name": "ScanLNK", "key": "scan_lnk", "limits": {"scanner": 10}},
-        "test_coordinate",
-    )
-
-    mocker.patch.object(ScanLNK, "upload_to_coordinator", return_value=None)
-    scanner.scan_wrapper(
-        Path(Path(__file__).parent / "fixtures/test.lnk").read_bytes(),
-        {"uid": "12345", "name": "somename"},
-        {"scanner_timeout": 5},
-        datetime.date.today(),
+    scanner_event = run_test_scan(
+        mocker=mocker,
+        scan_class=ScanUnderTest,
+        fixture_path=Path(__file__).parent / "fixtures/test.lnk",
     )
 
     TestCase.maxDiff = None
-    TestCase().assertDictEqual(test_scan_lnk_event, scanner.event)
+    TestCase().assertDictEqual(test_scan_event, scanner_event)
