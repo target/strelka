0190c84b377c51202051181f7370daa2202df3be
Ryan O'Horo <10855297+ryanohoro@users.noreply.github.com>
2022-12-20 22:07:44 -0600

    Test for ScanBase64PE

---
+++ src/python/strelka/tests/test_scan_base64_pe.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+from unittest import TestCase, mock
+
+from strelka.scanners.scan_base64_pe import ScanBase64PE as ScanUnderTest
+from strelka.tests import run_test_scan
+
+
+def test_scan_base64_pe(mocker):
+    """
+    Pass: Sample event matches output of scanner.
+    Failure: Unable to load file or sample event fails to match.
+    """
+
+    test_scan_event = {
+        "elapsed": mock.ANY,
+        "flags": [],
+        "decoded_header": b"MZ\x90\x00\x03\x00\x00\x00\x04\x00\x00\x00\xff\xff\x00\x00\xb8\x00\x00\x00\x00\x00\x00\x00@\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00",
+    }
+
+    scanner_event = run_test_scan(
+        mocker=mocker,
+        scan_class=ScanUnderTest,
+        fixture_path=Path(__file__).parent / "fixtures/test_pe.b64",
+    )
+
+    TestCase.maxDiff = None
+    TestCase().assertDictEqual(test_scan_event, scanner_event)
