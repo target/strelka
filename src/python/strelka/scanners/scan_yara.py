321cb2e461e2e1bfdb4632801f5cff7dda11fd01
Ryan O'Horo <10855297+ryanohoro@users.noreply.github.com>
2022-12-27 21:14:54 -0600

    Update default yara path in ScanYara

--- src/python/strelka/scanners/scan_yara.py
+++ src/python/strelka/scanners/scan_yara.py
@@ -20,48 +20,63 @@ class ScanYara(strelka.Scanner):
             (e.g. 'Author') that should be logged.
             Defaults to empty list.
     """
+
     def init(self):
         self.compiled_yara = None
 
     def scan(self, data, file, options, expire_at):
-        location = options.get('location', '/etc/yara/')
-        meta = options.get('meta', [])
+        location = options.get("location", "/etc/strelka/yara/")
+        meta = options.get("meta", [])
 
         try:
             if self.compiled_yara is None:
                 if os.path.isdir(location):
-                    globbed_yara_paths = glob.iglob(f'{location}/**/*.yar*', recursive=True)
-                    yara_filepaths = {f'namespace_{i}':entry for (i, entry) in enumerate(globbed_yara_paths)}
+                    globbed_yara_paths = glob.iglob(
+                        f"{location}/**/*.yar*", recursive=True
+                    )
+                    if not globbed_yara_paths:
+                        self.flags.append("yara_rules_not_found")
+                    yara_filepaths = {
+                        f"namespace_{i}": entry
+                        for (i, entry) in enumerate(globbed_yara_paths)
+                    }
                     self.compiled_yara = yara.compile(filepaths=yara_filepaths)
-                else:
+
+                elif os.path.isfile(location):
                     self.compiled_yara = yara.compile(filepath=location)
+                else:
+                    self.flags.append("yara_location_not_found")
 
-        except (yara.Error, yara.SyntaxError):
-            self.flags.append('compiling_error')
+        except yara.Error as e:
+            self.flags.append(f"compiling_error_general_{e}")
+        except yara.SyntaxError as e:
+            self.flags.append(f"compiling_error_syntax_{e}")
 
-        self.event['matches'] = []
-        self.event['tags'] = []
-        self.event['meta'] = []
+        self.event["matches"] = []
+        self.event["tags"] = []
+        self.event["meta"] = []
 
         try:
             if self.compiled_yara is not None:
                 yara_matches = self.compiled_yara.match(data=data)
                 for match in yara_matches:
-                    self.event['matches'].append(match.rule)
+                    self.event["matches"].append(match.rule)
                     if match.tags:
                         for tag in match.tags:
-                            if not tag in self.event['tags']:
-                                self.event['tags'].append(tag)
+                            if not tag in self.event["tags"]:
+                                self.event["tags"].append(tag)
 
                     for k, v in match.meta.items():
                         if meta and k not in meta:
                             continue
 
-                        self.event['meta'].append({
-                            'rule': match.rule,
-                            'identifier': k,
-                            'value': v,
-                        })
+                        self.event["meta"].append(
+                            {
+                                "rule": match.rule,
+                                "identifier": k,
+                                "value": v,
+                            }
+                        )
 
         except (yara.Error, yara.TimeoutError):
-            self.flags.append('scanning_error')
+            self.flags.append("scanning_error")
