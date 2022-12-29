b256534be814e473042836bf963c34285fbda4d0
Paul Hutelmyer <paul.hutelmyer@gmail.com>
2022-12-29 08:34:59 -0500

    Removing print statement

--- src/python/strelka/tests/test_scan_xml.py
+++ src/python/strelka/tests/test_scan_xml.py
@@ -10,7 +10,6 @@ def test_scan_xml(mocker):
     Pass: Sample event matches output of scanner.
     Failure: Unable to load file or sample event fails to match.
     """
-
     test_scan_event = {
         "elapsed": mock.ANY,
         "namespaces": [None],
@@ -39,6 +38,6 @@ def test_scan_xml(mocker):
         scan_class=ScanUnderTest,
         fixture_path=Path(__file__).parent / "fixtures/test.xml",
     )
-    print(scanner_event)
+
     TestCase.maxDiff = None
     TestCase().assertDictEqual(test_scan_event, scanner_event)
