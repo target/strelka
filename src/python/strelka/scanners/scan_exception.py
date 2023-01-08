bd22cbce97afde8d919e82708e4e302825f6f930
Ryan O'Horo <10855297+ryanohoro@users.noreply.github.com>
2023-01-07 21:58:01 -0600

    Add test for uncaught exceptions in Scanners

---
+++ src/python/strelka/scanners/scan_exception.py
@@ -0,0 +1,23 @@
+from strelka import strelka
+
+
+class ScanException(strelka.Scanner):
+    """Collects strings from files.
+
+    Collects strings from files (similar to the output of the Unix 'strings'
+    utility).
+
+    Options:
+        limit: Maximum number of strings to collect, starting from the
+            beginning of the file. If this value is 0, then all strings are
+            collected.
+            Defaults to 0 (unlimited).
+    """
+
+    def init(self):
+        pass
+
+    def scan(self, data, file, options, expire_at):
+        limit = options.get("limit", 0)
+
+        raise Exception("Scanner Exception")
