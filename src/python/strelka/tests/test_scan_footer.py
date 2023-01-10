4fac997bf98083821e1057ec0facdf09d12a1e54
Ryan O'Horo <10855297+ryanohoro@users.noreply.github.com>
2023-01-09 19:04:19 -0600

    Add new output encodings for ScanFooter/ScanHeader

--- src/python/strelka/tests/test_scan_footer.py
+++ src/python/strelka/tests/test_scan_footer.py
@@ -5,7 +5,7 @@ from strelka.scanners.scan_footer import ScanFooter as ScanUnderTest
 from strelka.tests import run_test_scan
 
 
-def test_scan_footer(mocker):
+def test_scan_footer_txt(mocker):
     """
     Pass: Sample event matches output of the scanner.
     Failure: Unable to load file or sample event fails to match.
@@ -15,12 +15,42 @@ def test_scan_footer(mocker):
         "elapsed": mock.ANY,
         "flags": [],
         "footer": b"itae. Et tortor consequat id porta nibh venenatis.",
+        "backslash": "itae. Et tortor consequat id porta nibh venenatis.",
+        "hex": b"697461652e20457420746f72746f7220636f6e73657175617420696420706f727461206e6962682076656e656e617469732e",
+        "raw": b"itae. Et tortor consequat id porta nibh venenatis.",
     }
 
     scanner_event = run_test_scan(
         mocker=mocker,
         scan_class=ScanUnderTest,
         fixture_path=Path(__file__).parent / "fixtures/test.txt",
+        options={"encodings": ["classic", "raw", "hex", "backslash"]},
+    )
+
+    TestCase.maxDiff = None
+    TestCase().assertDictEqual(test_scan_event, scanner_event)
+
+
+def test_scan_footer_exe(mocker):
+    """
+    Pass: Sample event matches output of the scanner.
+    Failure: Unable to load file or sample event fails to match.
+    """
+
+    test_scan_event = {
+        "elapsed": mock.ANY,
+        "flags": [],
+        "footer": b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00",
+        "backslash": "\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00",
+        "hex": b"0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
+        "raw": b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00",
+    }
+
+    scanner_event = run_test_scan(
+        mocker=mocker,
+        scan_class=ScanUnderTest,
+        fixture_path=Path(__file__).parent / "fixtures/test.exe",
+        options={"encodings": ["classic", "raw", "hex", "backslash"]},
     )
 
     TestCase.maxDiff = None
--- src/python/strelka/tests/test_scan_header.py
+++ src/python/strelka/tests/test_scan_header.py
@@ -15,12 +15,42 @@ def test_scan_header(mocker):
         "elapsed": mock.ANY,
         "flags": [],
         "header": b"Lorem ipsum dolor sit amet, consectetur adipiscing",
+        "backslash": "Lorem ipsum dolor sit amet, consectetur adipiscing",
+        "hex": b"4c6f72656d20697073756d20646f6c6f722073697420616d65742c20636f6e73656374657475722061646970697363696e67",
+        "raw": b"Lorem ipsum dolor sit amet, consectetur adipiscing",
     }
 
     scanner_event = run_test_scan(
         mocker=mocker,
         scan_class=ScanUnderTest,
         fixture_path=Path(__file__).parent / "fixtures/test.txt",
+        options={"encodings": ["classic", "raw", "hex", "backslash"]},
+    )
+
+    TestCase.maxDiff = None
+    TestCase().assertDictEqual(test_scan_event, scanner_event)
+
+
+def test_scan_header_exe(mocker):
+    """
+    Pass: Sample event matches output of the scanner.
+    Failure: Unable to load file or sample event fails to match.
+    """
+
+    test_scan_event = {
+        "elapsed": mock.ANY,
+        "flags": [],
+        "header": b"MZ\x90\x00\x03\x00\x00\x00\x04\x00\x00\x00\xff\xff\x00\x00\xb8\x00\x00\x00\x00\x00\x00\x00@\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00",
+        "backslash": "MZ\\x90\\x00\\x03\\x00\\x00\\x00\\x04\\x00\\x00\\x00\\xff\\xff\\x00\\x00\\xb8\\x00\\x00\\x00\\x00\\x00\\x00\\x00@\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00",
+        "hex": b"4d5a90000300000004000000ffff0000b8000000000000004000000000000000000000000000000000000000000000000000",
+        "raw": b"MZ\x90\x00\x03\x00\x00\x00\x04\x00\x00\x00\xff\xff\x00\x00\xb8\x00\x00\x00\x00\x00\x00\x00@\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00",
+    }
+
+    scanner_event = run_test_scan(
+        mocker=mocker,
+        scan_class=ScanUnderTest,
+        fixture_path=Path(__file__).parent / "fixtures/test.exe",
+        options={"encodings": ["classic", "raw", "hex", "backslash"]},
     )
 
     TestCase.maxDiff = None
