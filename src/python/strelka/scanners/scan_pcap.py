9acf9c3dc0b04be090c3ce52579657afbc93af32
Ryan O'Horo <10855297+ryanohoro@users.noreply.github.com>
2023-01-13 01:17:13 -0600

    ScanPcap with tests, new pcap tastes

---
+++ src/python/strelka/scanners/scan_pcap.py
@@ -0,0 +1,98 @@
+import json
+import os
+import shutil
+import subprocess
+import tempfile
+
+from strelka import strelka
+
+
+class ScanPcap(strelka.Scanner):
+    """Extract files from pcap/pcapng files.
+
+    Options:
+        limit: Maximum number of files to extract.
+            Defaults to 1000.
+    """
+    def scan(self, data, file, options, expire_at):
+        file_limit = options.get("limit", 1000)
+        tmp_directory = options.get("tmp_file_directory", "/tmp/")
+        scanner_timeout = options.get("scanner_timeout", 120)
+
+        self.event["total"] = {"files": 0, "extracted": 0}
+        self.event["files"] = []
+
+        try:
+            # Check if zeek package is installed
+            if not shutil.which("zeek"):
+                self.flags.append("zeek_not_installed_error")
+                return
+        except Exception as e:
+            self.flags.append(e)
+
+        with tempfile.NamedTemporaryFile(dir=tmp_directory, mode="wb") as tmp_data:
+            tmp_data.write(data)
+            tmp_data.flush()
+            tmp_data.seek(0)
+
+            with tempfile.TemporaryDirectory() as tmp_extract:
+
+                try:
+                    (stdout, stderr) = subprocess.Popen(
+                        ["zeek",
+                         "-r",
+                         tmp_data.name,
+                         "/opt/zeek/share/zeek/policy/frameworks/files/extract-all-files.zeek",
+                         f"FileExtract::prefix={tmp_extract}",
+                         "LogAscii::use_json=T"],
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.PIPE,
+                        cwd=tmp_extract
+                    ).communicate(timeout=scanner_timeout)
+
+                    if os.path.exists(os.path.join(tmp_extract, "files.log")):
+                        with open(os.path.join(tmp_extract, "files.log"), "r") as json_file:
+
+                            # files.log is one JSON object per line, convert to array
+                            file_events = json.loads("[" + ",".join(json_file.read().splitlines()) + "]")
+
+                            for file_event in file_events:
+
+                                if self.event["total"]["extracted"] >= file_limit:
+                                    self.flags.append("pcap_file_limit_error")
+                                    break
+
+                                self.event["total"]["files"] += 1
+                                self.event["files"].append(file_event)
+
+                                try:
+                                    print(os.path.join(tmp_extract, file_event["extracted"]))
+                                    if os.path.exists(os.path.join(tmp_extract, file_event["extracted"])):
+                                        self.upload(os.path.join(tmp_extract, file_event["extracted"]), expire_at)
+                                        self.event["total"]["extracted"] += 1
+
+                                except strelka.ScannerTimeout:
+                                    raise
+                                except Exception:
+                                    self.flags.append("zeek_file_upload_error")
+                    else:
+                        self.flags.append("zeek_no_file_log")
+
+                except strelka.ScannerTimeout:
+                    raise
+                except Exception:
+                    self.flags.append("zeek_extract_process_error")
+
+    def upload(self, name, expire_at):
+        """Send extracted file to coordinator"""
+        with open(name, "rb") as extracted_file:
+            extract_file = strelka.File(
+                source=self.name,
+            )
+
+            for c in strelka.chunk_string(extracted_file.read()):
+                self.upload_to_coordinator(
+                    extract_file.pointer,
+                    c,
+                    expire_at,
+                )
---
+++ src/python/strelka/tests/test_scan_pcap.py
@@ -0,0 +1,174 @@
+from pathlib import Path
+from pytest_unordered import unordered
+from unittest import TestCase, mock
+
+from strelka.scanners.scan_pcap import ScanPcap as ScanUnderTest
+from strelka.tests import run_test_scan
+
+
+def test_scan_pcap(mocker):
+    """
+    Pass: Sample event matches output of scanner.
+    Failure: Unable to load file or sample event fails to match.
+    """
+
+    test_scan_event = {
+        "elapsed": mock.ANY,
+        "flags": [],
+        "total": {"files": 3, "extracted": 3},
+        "files": [
+            {
+                "ts": 1673576655.41892,
+                "fuid": "FOxTJwn9u5H1hBXn1",
+                "tx_hosts": ["192.168.174.131"],
+                "rx_hosts": ["192.168.174.1"],
+                "conn_uids": mock.ANY,
+                "source": "HTTP",
+                "depth": 0,
+                "analyzers": unordered(["PE", "EXTRACT"]),
+                "mime_type": "application/x-dosexec",
+                "duration": 0.00018906593322753906,
+                "is_orig": False,
+                "seen_bytes": 4096,
+                "total_bytes": 4096,
+                "missing_bytes": 0,
+                "overflow_bytes": 0,
+                "timedout": False,
+                "extracted": "extract-1673576655.41892-HTTP-FOxTJwn9u5H1hBXn1",
+                "extracted_cutoff": False,
+            },
+            {
+                "ts": 1673576666.163778,
+                "fuid": "FxYAi61ktBsEM4hpNd",
+                "tx_hosts": ["192.168.174.131"],
+                "rx_hosts": ["192.168.174.1"],
+                "conn_uids": mock.ANY,
+                "source": "HTTP",
+                "depth": 0,
+                "analyzers": unordered(["EXTRACT"]),
+                "mime_type": "image/jpeg",
+                "duration": 0.007551908493041992,
+                "is_orig": False,
+                "seen_bytes": 308566,
+                "total_bytes": 308566,
+                "missing_bytes": 0,
+                "overflow_bytes": 0,
+                "timedout": False,
+                "extracted": "extract-1673576666.163778-HTTP-FxYAi61ktBsEM4hpNd",
+                "extracted_cutoff": False,
+            },
+            {
+                "ts": 1673576677.801391,
+                "fuid": "FoNGFk1uRR9pVo9XKi",
+                "tx_hosts": ["192.168.174.131"],
+                "rx_hosts": ["192.168.174.1"],
+                "conn_uids": mock.ANY,
+                "source": "HTTP",
+                "depth": 0,
+                "analyzers": unordered(["EXTRACT"]),
+                "mime_type": "application/xml",
+                "duration": 0.0,
+                "is_orig": False,
+                "seen_bytes": 620,
+                "total_bytes": 620,
+                "missing_bytes": 0,
+                "overflow_bytes": 0,
+                "timedout": False,
+                "extracted": "extract-1673576677.801391-HTTP-FoNGFk1uRR9pVo9XKi",
+                "extracted_cutoff": False,
+            },
+        ],
+    }
+
+    scanner_event = run_test_scan(
+        mocker=mocker,
+        scan_class=ScanUnderTest,
+        fixture_path=Path(__file__).parent / "fixtures/test.pcap",
+    )
+
+    TestCase.maxDiff = None
+    TestCase().assertDictEqual(test_scan_event, scanner_event)
+
+
+def test_scan_pcap_ng(mocker):
+    """
+    Pass: Sample event matches output of scanner.
+    Failure: Unable to load file or sample event fails to match.
+    """
+
+    test_scan_event = {
+        "elapsed": mock.ANY,
+        "flags": [],
+        "total": {"files": 3, "extracted": 3},
+        "files": [
+            {
+                "ts": 1673576655.41892,
+                "fuid": "FOxTJwn9u5H1hBXn1",
+                "tx_hosts": ["192.168.174.131"],
+                "rx_hosts": ["192.168.174.1"],
+                "conn_uids": mock.ANY,
+                "source": "HTTP",
+                "depth": 0,
+                "analyzers": unordered(["PE", "EXTRACT"]),
+                "mime_type": "application/x-dosexec",
+                "duration": 0.00018906593322753906,
+                "is_orig": False,
+                "seen_bytes": 4096,
+                "total_bytes": 4096,
+                "missing_bytes": 0,
+                "overflow_bytes": 0,
+                "timedout": False,
+                "extracted": "extract-1673576655.41892-HTTP-FOxTJwn9u5H1hBXn1",
+                "extracted_cutoff": False,
+            },
+            {
+                "ts": 1673576666.163778,
+                "fuid": "FxYAi61ktBsEM4hpNd",
+                "tx_hosts": ["192.168.174.131"],
+                "rx_hosts": ["192.168.174.1"],
+                "conn_uids": mock.ANY,
+                "source": "HTTP",
+                "depth": 0,
+                "analyzers": unordered(["EXTRACT"]),
+                "mime_type": "image/jpeg",
+                "duration": 0.007551908493041992,
+                "is_orig": False,
+                "seen_bytes": 308566,
+                "total_bytes": 308566,
+                "missing_bytes": 0,
+                "overflow_bytes": 0,
+                "timedout": False,
+                "extracted": "extract-1673576666.163778-HTTP-FxYAi61ktBsEM4hpNd",
+                "extracted_cutoff": False,
+            },
+            {
+                "ts": 1673576677.801391,
+                "fuid": "FoNGFk1uRR9pVo9XKi",
+                "tx_hosts": ["192.168.174.131"],
+                "rx_hosts": ["192.168.174.1"],
+                "conn_uids": mock.ANY,
+                "source": "HTTP",
+                "depth": 0,
+                "analyzers": unordered(["EXTRACT"]),
+                "mime_type": "application/xml",
+                "duration": 0.0,
+                "is_orig": False,
+                "seen_bytes": 620,
+                "total_bytes": 620,
+                "missing_bytes": 0,
+                "overflow_bytes": 0,
+                "timedout": False,
+                "extracted": "extract-1673576677.801391-HTTP-FoNGFk1uRR9pVo9XKi",
+                "extracted_cutoff": False,
+            },
+        ],
+    }
+
+    scanner_event = run_test_scan(
+        mocker=mocker,
+        scan_class=ScanUnderTest,
+        fixture_path=Path(__file__).parent / "fixtures/test.pcapng"
+    )
+
+    TestCase.maxDiff = None
+    TestCase().assertDictEqual(test_scan_event, scanner_event)
