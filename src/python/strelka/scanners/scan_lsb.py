9c0c566493ddebc38915c7a3923e7a4a8d5908c7
Ryan O'Horo <10855297+ryanohoro@users.noreply.github.com>
2023-01-23 20:34:57 -0600

    Exception handling improvements in ScanNf and ScanLsb

--- src/python/strelka/scanners/scan_lsb.py
+++ src/python/strelka/scanners/scan_lsb.py
@@ -8,24 +8,29 @@ class ScanLsb(strelka.Scanner):
     """This scanner checks if there is any hidden strings at the end of each RGB value"""
 
     def scan(self,data,file,options, expire_at):
-        ans=False
-        image = np.fromstring(data, np.uint8)
-        image = cv2.imdecode(image, cv2.IMREAD_COLOR)
-        bits = self._get_bits(image)
-        bytes_ = self._get_bytes(bits)
-        chars = []
-        chars.append(self._convert_bytes_to_text(bytes_))
-        flag=(''.join(chars).encode('ascii', 'ignore'))
-        if (len(flag)>1):
-            ans=True
-            self.event['lsb'] = ans
-            #print("This Image might have something stored in")
-        else:
-            extract_file = strelka.File (
-                source = self.name
-            )
-            self.event['lsb'] = ans
-    
+        try:
+            ans=False
+            image = np.fromstring(data, np.uint8)
+            image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+            bits = self._get_bits(image)
+            bytes_ = self._get_bytes(bits)
+            chars = []
+            chars.append(self._convert_bytes_to_text(bytes_))
+            flag=(''.join(chars).encode('ascii', 'ignore'))
+            if (len(flag)>1):
+                ans=True
+                self.event['lsb'] = ans
+                #print("This Image might have something stored in")
+            else:
+                extract_file = strelka.File (
+                    source = self.name
+                )
+                self.event['lsb'] = ans
+        except AttributeError:
+            self.flags.append("bits_image_error")
+        except cv2.error:
+            self.flags.append("cv2_image_error")
+
     def _get_bits(self, img):
         h, w, t = img.shape
         bits = ''
