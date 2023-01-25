fd87e13d9a20ee3dfff12a59abcead2e3f109ad5
Paul Hutelmyer <paul.hutelmyer@gmail.com>
2023-01-25 09:14:10 -0500

    Bug fixes + formatting

--- src/python/strelka/auxiliary/xl4ma/analyzer.py
+++ src/python/strelka/auxiliary/xl4ma/analyzer.py
@@ -22,15 +22,15 @@ def _make_temp_file(data, file_type):
 def _get_file_type(data):
     file_type = None
 
-    if data[:2] == b'\xd0\xcf':
-        file_type = 'xls'
-    elif data[:2] == b'\x50\x4b':
-        file_type = 'xlsx'
-    if file_type == 'xlsx':
-        if bytes('workbook.bin', 'ascii') in data:
-            file_type = 'xlsb'
-        if bytes('workbook.xml', 'ascii') in data:
-            file_type = 'xlsm'
+    if data[:2] == b"\xd0\xcf":
+        file_type = "xls"
+    elif data[:2] == b"\x50\x4b":
+        file_type = "xlsx"
+    if file_type == "xlsx":
+        if bytes("workbook.bin", "ascii") in data:
+            file_type = "xlsb"
+        if bytes("workbook.xml", "ascii") in data:
+            file_type = "xlsm"
 
     temp_file = _make_temp_file(data, file_type)
 
@@ -44,27 +44,24 @@ def process_data(data, filename):
 
     results = dict()
 
-    if file_type == 'xls':
+    if file_type == "xls":
         excel_doc = XLSWrapper(file_path)
-    elif file_type == 'xlsb':
+    elif file_type == "xlsb":
         excel_doc = XLSBWrapper(file_path)
-    elif file_type == 'xlsm':
+    elif file_type == "xlsm":
         excel_doc = XLSMWrapper(file_path)
 
-    if not hasattr(excel_doc, 'workbook'):
-        logging.debug('file not supported')
+    if not hasattr(excel_doc, "workbook"):
+        logging.debug("file not supported")
         return
 
     results.update(excel_doc.parse_sheets(file_path))
-    results['meta'].update({
-        'file_name': filename,
-        'file_type': file_type
-    })
+    results["meta"].update({"file_name": filename, "file_type": file_type})
 
-    excel_doc_decoded = decode(file_path, file_type, results['defined_names'])
+    excel_doc_decoded = decode(file_path, file_type, results["defined_names"])
 
-    results['decoded'] = excel_doc_decoded
-    results['iocs'] = iocs(excel_doc_decoded)
+    results["decoded"] = excel_doc_decoded
+    results["iocs"] = iocs(excel_doc_decoded)
 
     temp_file.close()
     os.unlink(temp_file.name)
@@ -73,10 +70,15 @@ def process_data(data, filename):
 
 
 def parse_args(args=None):
-    parser = argparse.ArgumentParser(
-        description='Excel4 Macro Analyzer'
+    parser = argparse.ArgumentParser(description="Excel4 Macro Analyzer")
+    parser.add_argument(
+        "--file",
+        required=True,
+        type=str,
+        action="store",
+        metavar="FILE_PATH",
+        help="path to file",
     )
-    parser.add_argument('--file', required=True, type=str, action='store', metavar='FILE_PATH', help="path to file")
 
     return parser.parse_args(args)
 
@@ -84,7 +86,7 @@ def parse_args(args=None):
 def main(args=None):
     args = parse_args(args)
 
-    with open(args.file, 'rb') as fd:
+    with open(args.file, "rb") as fd:
         data = fd.read()
 
     process_data(data, filename=Path(args.file).name)
--- src/python/strelka/auxiliary/xl4ma/xlsm_wrapper.py
+++ src/python/strelka/auxiliary/xl4ma/xlsm_wrapper.py
@@ -5,7 +5,6 @@ from openpyxl import load_workbook
 
 
 class XLSMWrapper:
-
     def __init__(self, file_path):
         try:
             self.workbook = load_workbook(file_path, read_only=False, keep_vba=True)
@@ -20,9 +19,7 @@ class XLSMWrapper:
         return defined_names
 
     def parse_sheets(self, file_path):
-        results = {
-            'sheets': []
-        }
+        results = {"sheets": []}
         formula_count = 0
         value_count = 0
         sheet_count = 0
@@ -36,41 +33,40 @@ class XLSMWrapper:
                     if cell.value:
                         if cell.data_type == "f":
                             formula_count += 1
-                            formulas.append({
-                                "cell": cell.coordinate,
-                                "value": cell.value
-                            })
+                            formulas.append(
+                                {"cell": cell.coordinate, "value": cell.value}
+                            )
                             if re.match("^=?\w+?\(\)$", cell.value):
-                                worksheet = self.workbook.get_sheet_by_name(sheet)
+                                worksheet = self.workbook[sheet]
                                 worksheet[cell.coordinate] = ""
                         elif cell.data_type == "n":
                             value_count += 1
-                            values.append({
-                                "cell": cell.coordinate,
-                                "value": cell.value
-                            })
+                            values.append(
+                                {"cell": cell.coordinate, "value": cell.value}
+                            )
                         elif cell.data_type == "s":
                             value_count += 1
-                            values.append({
-                                "cell": cell.coordinate,
-                                "value": cell.value
-                            })
+                            values.append(
+                                {"cell": cell.coordinate, "value": cell.value}
+                            )
 
-            results['sheets'].append({
-                # 'sheet_number': sheet.sheetId,
-                'sheet_name': sheet,
-                # 'sheet_type': sheet.type.upper(),
-                'visibility': self.workbook[sheet].sheet_state.upper(),
-                'formulas': formulas,
-                'values': values,
-            })
+            results["sheets"].append(
+                {
+                    # 'sheet_number': sheet.sheetId,
+                    "sheet_name": sheet,
+                    # 'sheet_type': sheet.type.upper(),
+                    "visibility": self.workbook[sheet].sheet_state.upper(),
+                    "formulas": formulas,
+                    "values": values,
+                }
+            )
 
-        results['defined_names'] = self.get_defined_names()
-        results['meta'] = {
-            'formulas': formula_count,
-            'values': value_count,
-            'sheets': sheet_count,
-            'defined_names': len(results['defined_names'])
+        results["defined_names"] = self.get_defined_names()
+        results["meta"] = {
+            "formulas": formula_count,
+            "values": value_count,
+            "sheets": sheet_count,
+            "defined_names": len(results["defined_names"]),
         }
 
         self.workbook.save(file_path)
