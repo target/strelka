3598e2e137dbf506bf143e4a3e8c6c6022dfc8a6
Paul Hutelmyer <paul.hutelmyer@gmail.com>
2023-01-26 17:31:05 -0500

    Updating formatting

--- src/python/strelka/scanners/scan_javascript.py
+++ src/python/strelka/scanners/scan_javascript.py
@@ -12,27 +12,28 @@ class ScanJavascript(strelka.Scanner):
             deobfuscated.
             Defaults to True.
     """
-    def scan(self, data, file, options, expire_at):
-        beautify = options.get('beautify', True)
-        limits = options.get('limits', 50)
 
-        self.event.setdefault('tokens', set())
-        self.event.setdefault('keywords', set())
-        self.event.setdefault('strings', set())
-        self.event.setdefault('identifiers', set())
-        self.event.setdefault('regular_expressions', set())
-        self.event['beautified'] = False
+    def scan(self, data, file, options, expire_at):
+        beautify = options.get("beautify", True)
+        limits = options.get("limits", 50)
+
+        self.event.setdefault("tokens", set())
+        self.event.setdefault("keywords", set())
+        self.event.setdefault("strings", set())
+        self.event.setdefault("identifiers", set())
+        self.event.setdefault("regular_expressions", set())
+        self.event["beautified"] = False
 
         js = None
 
         try:
             if beautify:
                 js = jsbeautifier.beautify(data.decode())
-                self.event['beautified'] = True
+                self.event["beautified"] = True
         except strelka.ScannerTimeout:
             raise
         except Exception:
-            self.flags.append('beautify_failed')
+            self.flags.append("beautify_failed")
 
         try:
             if js is None:
@@ -40,39 +41,41 @@ class ScanJavascript(strelka.Scanner):
         except strelka.ScannerTimeout:
             raise
         except Exception:
-            self.flags.append('decode_failed')
+            self.flags.append("decode_failed")
 
         try:
             tokens = esprima.tokenize(
                 js,
                 options={
-                    'comment': True,
-                    'tolerant': True,
-                }
+                    "comment": True,
+                    "tolerant": True,
+                },
             )
             for t in tokens:
-                if t.type not in self.event['tokens']:
-                    self.event['tokens'].add(t.type)
-                if t.type == 'String':
-                    stripped_val = t.value.strip('"\'')
-                    if stripped_val not in self.event['strings']:
-                        self.event['strings'].add(stripped_val)
-                if t.type == 'Keyword':
-                    if t.value not in self.event['keywords']:
-                        self.event['keywords'].add(t.value)
-                if t.type == 'Identifier':
-                    if t.value not in self.event['identifiers']:
-                        self.event['identifiers'].add(t.value)
-                if t.type == 'RegularExpression':
-                    if t.value not in self.event['regular_expressions']:
-                        self.event['regular_expressions'].add(t.value)
+                if t.type not in self.event["tokens"]:
+                    self.event["tokens"].add(t.type)
+                if t.type == "String":
+                    stripped_val = t.value.strip("\"'")
+                    if stripped_val not in self.event["strings"]:
+                        self.event["strings"].add(stripped_val)
+                if t.type == "Keyword":
+                    if t.value not in self.event["keywords"]:
+                        self.event["keywords"].add(t.value)
+                if t.type == "Identifier":
+                    if t.value not in self.event["identifiers"]:
+                        self.event["identifiers"].add(t.value)
+                if t.type == "RegularExpression":
+                    if t.value not in self.event["regular_expressions"]:
+                        self.event["regular_expressions"].add(t.value)
 
-            self.event['tokens'] = list(self.event["tokens"])[:limits]
-            self.event['keywords'] = list(self.event["keywords"])[:limits]
-            self.event['strings'] = list(self.event["strings"])[:limits]
-            self.event['identifiers'] = list(self.event["identifiers"])[:limits]
-            self.event['regular_expressions'] = list(self.event["regular_expressions"])[:limits]
+            self.event["tokens"] = list(self.event["tokens"])[:limits]
+            self.event["keywords"] = list(self.event["keywords"])[:limits]
+            self.event["strings"] = list(self.event["strings"])[:limits]
+            self.event["identifiers"] = list(self.event["identifiers"])[:limits]
+            self.event["regular_expressions"] = list(self.event["regular_expressions"])[
+                :limits
+            ]
         except strelka.ScannerTimeout:
             raise
         except Exception:
-            self.flags.append('tokenization_failed')
+            self.flags.append("tokenization_failed")
