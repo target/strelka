38d098fb9a3d3ebcfee5caadcf246ec54a997a80
Ryan O'Horo <10855297+ryanohoro@users.noreply.github.com>
2023-02-08 16:16:38 -0600

    Update requirements for otel

--- src/python/strelka/telemetry/__init__.py
+++ src/python/strelka/telemetry/__init__.py
@@ -10,7 +10,7 @@ import strelka
 def get_resource(meta: Optional[dict] = None):
     attributes = {
         "service.namespace": strelka.__namespace__,
-        "service.name": "strelka.backend",
+        "service.name": "strelka.backend.worker",
         "service.version": strelka.__version__,
         "host.name": os.environ.get("HOSTNAME", None),
         "host.arch": platform.processor(),
