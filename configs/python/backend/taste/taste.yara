f0fa0b005407a92ca7ce12b6c41cee58a2377619
Paul Hutelmyer <paul.hutelmyer@gmail.com>
2023-03-07 11:45:22 -0500

    Initial submission

--- configs/python/backend/taste/taste.yara
+++ configs/python/backend/taste/taste.yara
@@ -905,3 +905,17 @@ rule credit_cards {
     condition:
         any of them
 }
+
+rule vsto_file
+{
+    meta:
+        description = "Detects Microsoft Office VSTO files with a specific format"
+        reference = "https://www.deepinstinct.com/blog/no-macro-no-worries-vsto-being-weaponized-by-threat-actors"
+        type = "text"
+    strings:
+        $ = /<vt:lpwstr>\w+\.vsto\|[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}\|vstolocal<\/vt:lpwstr>/
+        $ = "urn:schemas-microsoft-com:asm.v1"
+    condition:
+        magic.mime_type() == "text/xml" and
+        any of them
+}
---
+++ src/python/strelka/scanners/scan_vsto.py
@@ -0,0 +1,77 @@
+"""
+This module provides a scanner that extracts information from VSTO files.
+
+It defines the following class:
+    - ScanVsto: Scans VSTO files and extracts information like the name, assembly identity, dependencies,
+                publisher, and certificate.
+
+"""
+
+
+import base64
+import hashlib
+import xmltodict
+from strelka import strelka
+
+
+class ScanVsto(strelka.Scanner):
+    """
+    Scanner class for extracting information from VSTO files.
+
+    This class provides a `scan` method that extracts information from VSTO files, and stores it in the `event`
+    dictionary attribute of the class.
+
+    """
+
+    def scan(self, data, file, options, expire_at):
+        """
+        Extracts information from the VSTO file.
+
+        Args:
+            data: The binary data of the VSTO file to be scanned.
+            file: File associated with data.
+            options: Any options passed in from the backend configuration file.
+            expire_at: The expiry time for this scan.
+
+        """
+        try:
+            # As Vsto is in an XML format, parse the XML data
+            xml = xmltodict.parse(data)
+
+            # Extract the VSTO name
+            if props := xml.get("Properties"):
+                for prop in props.get("property", []):
+                    if prop["vt:lpwstr"].endswith("vstolocal"):
+                        self.event["vsto"] = prop["vt:lpwstr"].split("|")[0]
+
+            # Extract the assembly identity, dependencies, publisher, and certificate information
+            if asm := xml.get("asmv1:assembly"):
+                if asm.get("assemblyIdentity"):
+                    self.event["identity"] = asm["assemblyIdentity"]["@name"]
+                    self.event["dependency"] = {
+                        "manifest": asm["dependency"]["dependentAssembly"]["@codebase"],
+                        "name": asm["dependency"]["dependentAssembly"][
+                            "assemblyIdentity"
+                        ]["@name"],
+                    }
+                    self.event["publisher"] = asm["publisherIdentity"]["@name"]
+                    self.event["certificate"] = {
+                        "b64": asm["Signature"]["KeyInfo"]["msrel:RelData"][
+                            "r:license"
+                        ]["r:issuer"]["Signature"]["KeyInfo"]["X509Data"][
+                            "X509Certificate"
+                        ],
+                        "md5": hashlib.md5(
+                            base64.b64decode(
+                                asm["Signature"]["KeyInfo"]["msrel:RelData"][
+                                    "r:license"
+                                ]["r:issuer"]["Signature"]["KeyInfo"]["X509Data"][
+                                    "X509Certificate"
+                                ]
+                            )
+                        ).hexdigest(),
+                    }
+
+        except Exception as e:
+            print(e)
+            self.flags.append(f"{self.__class__.__name__} Exception: {str(e)[:100]}")
