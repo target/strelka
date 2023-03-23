9293b2ce1624eb6795ccca15e961cf08817bf7c0
Derekt2 <derek.e.thomas11@gmail.com>
2023-03-23 14:09:50 -0500

    Update scan_manifest.py
    This flatten nested dictionaries within a list 1 level deep, and ensures the only processed values in an array are strings.

--- src/python/strelka/scanners/scan_manifest.py
+++ src/python/strelka/scanners/scan_manifest.py
@@ -1,30 +1,34 @@
+from strelka import strelka
 import json
 
-from strelka import strelka
-
+def flatten(input: list) -> list:
+    for i in input:
+        if isinstance(i, dict):
+            for key, value in i.items():
+                if isinstance(value, list):
+                    for val in value:
+                        input.append(str(key) + "_" + str(val))
+    input = [i for i in input if isinstance(i, str)]
+    return input
 
 class ScanManifest(strelka.Scanner):
-    """Parses browser extension's  manifest.json."""
-
+    """Parses browser extension's  manifest.json.
+    """
     def scan(self, data, file, options, expire_at):
         try:
             jsondata = json.loads(data)
-            required_keys = ["name", "manifest_version", "version"]
-            optional_keys = [
-                "content_scripts",
-                "content_security_policy",
-                "description",
-                "permissions",
-                "update_url",
-                "key",
-            ]
+            required_keys = ['name', 'manifest_version', 'version']
+            optional_keys = ['content_scripts', 'content_security_policy', 'description', 'permissions', 'update_url', 'key']
             for key in required_keys:
-                self.event[key] = jsondata[key]
+                self.event[key] = str(jsondata[key])
             for key in optional_keys:
                 if jsondata.get(key):
-                    self.event[key] = jsondata[key]
+                    if isinstance(jsondata[key], list):
+                        self.event[key] = flatten(jsondata[key])
+                    else:
+                        self.event[key] = jsondata[key]
         except strelka.ScannerTimeout:
             raise
         except Exception:
-            self.flags.append("error parsing manifest")
+            self.flags.append('error parsing manifest')
             return
