20aaa19b319917b3691640f394b2f9fcd1e9eb6a
Paul Hutelmyer <paul.hutelmyer@gmail.com>
2023-03-28 07:55:58 -0400

    Removing mmbot references

--- src/python/setup.py
+++ src/python/setup.py
@@ -10,7 +10,7 @@ setuptools.setup(
     description="strelka: container-based file analysis at scale",
     license="Apache 2.0",
     packages=setuptools.find_packages(),
-    scripts=["bin/strelka-backend", "bin/strelka-mmrpc"],
+    scripts=["bin/strelka-backend"],
     zip_safe=False,
     entry_points={
         "console_scripts": [
