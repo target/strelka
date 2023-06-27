a990664d1c85598ce25e8e8a26a652ba4ec3e9e4
Alex Kahan <alex@sublimesecurity.com>
2023-06-27 13:06:31 -0400

    Style

--- src/python/strelka/scanners/scan_ocr.py
+++ src/python/strelka/scanners/scan_ocr.py
@@ -23,9 +23,9 @@ class ScanOcr(strelka.Scanner):
         tmp_directory = options.get("tmp_directory", "/tmp/")
         pdf_to_png = options.get('pdf_to_png', False)
 
-        if pdf_to_png and 'application/pdf' in file.flavors.get('mime', []):
-            doc = fitz.open(stream=data, filetype='pdf')
-            data = doc.get_page_pixmap(0).tobytes('png')
+        if pdf_to_png and "application/pdf" in file.flavors.get("mime", []):
+            doc = fitz.open(stream=data, filetype="pdf")
+            data = doc.get_page_pixmap(0).tobytes("png")
 
         with tempfile.NamedTemporaryFile(dir=tmp_directory) as tmp_data:
             tmp_data.write(data)
