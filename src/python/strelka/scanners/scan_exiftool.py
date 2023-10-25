9821da3dc77bc9e553e200af068d39a2aee0d4d2
Paul Hutelmyer <paul.hutelmyer@gmail.com>
2023-10-25 14:04:20 -0400

    Updating exiftool scanner / test

--- src/python/strelka/scanners/scan_exiftool.py
+++ src/python/strelka/scanners/scan_exiftool.py
@@ -1,55 +1,54 @@
-import ast
 import json
 import subprocess
 import tempfile
 
 from strelka import strelka
 
-
 class ScanExiftool(strelka.Scanner):
     """Collects metadata parsed by Exiftool.
 
+    This scanner uses Exiftool to extract metadata from files and logs the
+    extracted key-value pairs.
+
     Options:
-        keys: exiftool key values to log in the event.
-            Defaults to all.
         tmp_directory: Location where tempfile writes temporary files.
             Defaults to '/tmp/'.
     """
 
     def scan(self, data, file, options, expire_at):
-        keys = options.get("keys", [])
         tmp_directory = options.get("tmp_directory", "/tmp/")
 
+        # Use a temporary file to store the data for processing with Exiftool
         with tempfile.NamedTemporaryFile(dir=tmp_directory) as tmp_data:
             tmp_data.write(data)
             tmp_data.flush()
 
-            (stdout, stderr) = subprocess.Popen(
-                ["exiftool", "-d", '"%s"', "-j", tmp_data.name],
-                stdout=subprocess.PIPE,
-                stderr=subprocess.DEVNULL,
-            ).communicate()
+            try:
+                # Execute exiftool and retrieve JSON metadata output
+                (stdout, stderr) = subprocess.Popen(
+                    ["exiftool", "-j", tmp_data.name],
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.DEVNULL,
+                ).communicate()
 
-            if stdout:
-                exiftool_dictionary = json.loads(stdout)[0]
+                if stdout:
+                    # Load metadata from stdout and update the event dictionary with it
+                    # Converts fields with spaces to underscores to accommodate
+                    # searchability (i.e.,  "File Name" to "file_name")
+                    metadata = json.loads(stdout)[0]
+                    for key, value in metadata.items():
+                        formatted_key = key.replace(" ", "_").replace("/", "_").lower()
 
-                self.event["keys"] = []
-                for k, v in exiftool_dictionary.items():
-                    if keys and k not in keys:
-                        continue
+                        # Convert any lists to a comma-separated string
+                        if isinstance(value, list):
+                            value = ", ".join(map(str, value))
 
-                    if isinstance(v, str):
-                        v = v.strip()
-                        v = v.strip("'\"")
+                        self.event[formatted_key] = value
 
-                        try:
-                            v = ast.literal_eval(v)
-                        except (ValueError, SyntaxError):
-                            pass
-
-                    self.event["keys"].append(
-                        {
-                            "key": k,
-                            "value": v,
-                        }
-                    )
+            # Handle potential errors from exiftool and JSON decoding
+            except subprocess.CalledProcessError as e:
+                self.flags.append(f"exiftool_error: Subprocess Error - {str(e)}")
+            except json.JSONDecodeError as e:
+                self.flags.append(f"exiftool_error: JSON Decode Error - {str(e)}")
+            except Exception as e:
+                self.flags.append(f"exiftool_error: General Error - {str(e)}")
