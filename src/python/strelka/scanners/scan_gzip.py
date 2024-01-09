fe354ce3d4366f6a3faebaad3691c8396767de54
Paul Hutelmyer <paul.hutelmyer@gmail.com>
2024-01-09 08:16:21 -0500

    Additional error handling

--- src/python/strelka/scanners/scan_gzip.py
+++ src/python/strelka/scanners/scan_gzip.py
@@ -1,5 +1,6 @@
 import gzip
 import io
+import zlib
 
 from strelka import strelka
 
@@ -8,10 +9,17 @@ class ScanGzip(strelka.Scanner):
     """Decompresses gzip files."""
 
     def scan(self, data, file, options, expire_at):
-        with io.BytesIO(data) as gzip_io:
-            with gzip.GzipFile(fileobj=gzip_io) as gzip_obj:
-                decompressed = gzip_obj.read()
-                self.event["size"] = len(decompressed)
+        try:
+            with io.BytesIO(data) as gzip_io:
+                with gzip.GzipFile(fileobj=gzip_io) as gzip_obj:
+                    decompressed = gzip_obj.read()
+                    self.event["size"] = len(decompressed)
 
-                # Send extracted file back to Strelka
-                self.emit_file(decompressed, name=file.name)
+                    # Send extracted file back to Strelka
+                    self.emit_file(decompressed, name=file.name)
+        except gzip.BadGzipFile:
+            self.flags.append("bad_gzip_file")
+        except zlib.error:
+            self.flags.append("bad_gzip_file")
+        except EOFError:
+            self.flags.append("eof_error")
