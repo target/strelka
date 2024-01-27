3cb1408c5ca324d7fe977c9b96b31721e184d318
Ryan O'Horo <10855297+ryanohoro@users.noreply.github.com>
2024-01-27 01:34:57 -0600

    ScanZip - Add support for mixed crypto and aes256

--- src/python/strelka/scanners/scan_zip.py
+++ src/python/strelka/scanners/scan_zip.py
@@ -24,9 +24,11 @@ class ScanZip(strelka.Scanner):
         file_limit = options.get("limit", 100)
         size_limit = options.get("size_limit", 250000000)
         limit_metadata = options.get("limit_metadata", True)
+        crack_pws = options.get("crack_pws", False)
+        log_pws = options.get("log_pws", True)
         password_file = options.get("password_file", "/etc/strelka/passwords.dat")
 
-        passwords = []
+        passwords = [None]
 
         # Gather count and list of files to be extracted
         self.event["total"] = {"files": 0, "extracted": 0}
@@ -36,100 +38,99 @@ class ScanZip(strelka.Scanner):
         compress_size_total = 0
         file_size_total = 0
 
-        if os.path.isfile(password_file):
+        if crack_pws and os.path.isfile(password_file):
             with open(password_file, "rb") as f:
                 for line in f:
                     passwords.append(line.strip())
 
         with io.BytesIO(data) as zip_io:
             try:
-                is_aes = False
-                with pyzipper.ZipFile(zip_io) as zip_obj:
-                    filelist = zip_obj.filelist
-                    for file in filelist:
-                        if not file.is_dir():
-                            # Check for the AES compression type
-                            if file.compress_type == 99:
-                                is_aes = True
-                                break
-
-                with pyzipper.ZipFile(zip_io) if is_aes else pyzipper.ZipFile(
-                    zip_io
-                ) as zip_obj:
+                with pyzipper.AESZipFile(zip_io) as zip_obj:
                     filelist = zip_obj.filelist
 
                     # Count the file entries, in case the function encounters an unhandled exception
-                    for file in filelist:
-                        if file.is_dir():
+                    for compressed_file in filelist:
+                        if compressed_file.is_dir():
                             continue
                         self.event["total"]["files"] += 1
 
                     # For each file in zip, gather metadata and pass extracted file back to Strelka
-                    for file in filelist:
-                        if file.is_dir():
+                    for compressed_file in filelist:
+                        if compressed_file.is_dir():
                             continue
 
                         extract = True
                         extracted = False
                         compression_rate = 0
 
-                        if file.file_size > 0 and file.compress_size > 0:
-                            compress_size_total += file.compress_size
-                            file_size_total += file.file_size
+                        if compressed_file.file_size > size_limit:
+                            extract = False
+                            if "file_size_limit" not in self.flags:
+                                self.flags.append("file_size_limit")
 
-                            size_difference = file.file_size - file.compress_size
+                        if self.event["total"]["extracted"] >= file_limit:
+                            extract = False
+                            if "file_count_limit" not in self.flags:
+                                self.flags.append("file_count_limit")
+
+                        if (
+                            compressed_file.file_size > 0
+                            and compressed_file.compress_size > 0
+                        ):
+                            compress_size_total += compressed_file.compress_size
+                            file_size_total += compressed_file.file_size
+
+                            size_difference = (
+                                compressed_file.file_size
+                                - compressed_file.compress_size
+                            )
                             compression_rate = (
                                 size_difference * 100.0
-                            ) / file.file_size
+                            ) / compressed_file.file_size
 
                         try:
                             extract_data = b""
-                            zinfo = zip_obj.getinfo(file.filename)
+                            zinfo = zip_obj.getinfo(compressed_file.filename)
 
                             if zinfo.flag_bits & 0x1:
-                                #  If it's encrypted, don't extract file to coordinator, let ScanEncryptedZip do it instead
-                                extract = False
                                 if "encrypted" not in self.flags:
                                     self.flags.append("encrypted")
 
-                                if passwords:
-                                    for pw in passwords:
-                                        try:
-                                            extract_data = zip_obj.read(
-                                                file.filename, pw
-                                            )
-                                            self.event["password"] = pw.decode("utf-8")
-
-                                        except (
-                                            RuntimeError,
-                                            pyzipper.BadZipFile,
-                                            zlib.error,
-                                        ):
-                                            pass
-                            else:
+                            for password in passwords:
+                                print("Trying ", password)
                                 try:
-                                    extract_data = zip_obj.read(file.filename)
-                                except RuntimeError:
-                                    self.flags.append("runtime_error")
-                                except pyzipper.BadZipFile:
-                                    self.flags.append("bad_zip_file")
-                                except zlib.error:
-                                    self.flags.append("zlib_error")
-
-                            if file.file_size > size_limit:
-                                extract = False
-                                if "file_size_limit" not in self.flags:
-                                    self.flags.append("file_size_limit")
-
-                            if self.event["total"]["extracted"] >= file_limit:
-                                extract = False
-                                if "file_count_limit" not in self.flags:
-                                    self.flags.append("file_count_limit")
+                                    if extract:
+                                        print(
+                                            compressed_file.filename,
+                                            compressed_file.compress_type,
+                                            password,
+                                        )
+                                        extract_data = zip_obj.read(
+                                            compressed_file.filename, password
+                                        )
+                                        if extract_data:
+                                            print(extract_data[:25])
+                                            if password and crack_pws and log_pws:
+                                                if "password" not in self.event.keys():
+                                                    self.event["password"] = []
+                                                if password.decode(
+                                                    "utf-8"
+                                                ) not in self.event.get("password", []):
+                                                    self.event["password"].append(
+                                                        password.decode("utf-8")
+                                                    )
+                                            break
+                                        else:
+                                            print("no data")
+                                except RuntimeError as e:
+                                    print("Error:", e)
 
                             # If there's data in it, and no limits have been met, emit the file
                             if extract_data and extract:
                                 # Send extracted file back to Strelka
-                                self.emit_file(extract_data, name=file.filename)
+                                self.emit_file(
+                                    extract_data, name=compressed_file.filename
+                                )
                                 extracted = True
 
                             if not (
@@ -138,11 +139,14 @@ class ScanZip(strelka.Scanner):
                             ):
                                 self.event["files"].append(
                                     {
-                                        "file_name": file.filename,
-                                        "file_size": file.file_size,
-                                        "compression_size": file.compress_size,
+                                        "file_name": compressed_file.filename,
+                                        "file_size": compressed_file.file_size,
+                                        "compression_size": compressed_file.compress_size,
                                         "compression_rate": round(compression_rate, 2),
                                         "extracted": extracted,
+                                        "encrypted": True
+                                        if zinfo.flag_bits & 0x1 == 1
+                                        else False,
                                     }
                                 )
 
@@ -157,15 +161,19 @@ class ScanZip(strelka.Scanner):
                             self.flags.append("value_error")
                         except zlib.error:
                             self.flags.append("zlib_error")
+                        except pyzipper.BadZipFile:
+                            self.flags.append("bad_zip_file")
 
-                    # Top level compression metric
-                    if file_size_total > 0 and compress_size_total > 0:
-                        size_difference_total = file_size_total - compress_size_total
-                        self.event["compression_rate"] = round(
-                            (size_difference_total * 100.0) / file_size_total, 2
-                        )
-                    else:
-                        self.event["compression_rate"] = 0.00
+                        # Top level compression metric
+                        if file_size_total > 0 and compress_size_total > 0:
+                            size_difference_total = (
+                                file_size_total - compress_size_total
+                            )
+                            self.event["compression_rate"] = round(
+                                (size_difference_total * 100.0) / file_size_total, 2
+                            )
+                        else:
+                            self.event["compression_rate"] = 0.00
 
             except pyzipper.BadZipFile:
                 self.flags.append("bad_zip_file")
--- src/python/strelka/tests/test_scan_zip.py
+++ src/python/strelka/tests/test_scan_zip.py
@@ -22,6 +22,7 @@ def test_scan_zip(mocker):
                 "compression_size": 1425,
                 "compression_rate": 64.51,
                 "extracted": True,
+                "encrypted": False,
             },
             {
                 "file_name": "hidden/lorem-readonly.txt",
@@ -29,6 +30,7 @@ def test_scan_zip(mocker):
                 "compression_size": 1425,
                 "compression_rate": 64.51,
                 "extracted": True,
+                "encrypted": False,
             },
             {
                 "file_name": "hidden/lorem.txt",
@@ -36,6 +38,7 @@ def test_scan_zip(mocker):
                 "compression_size": 1425,
                 "compression_rate": 64.51,
                 "extracted": True,
+                "encrypted": False,
             },
             {
                 "file_name": "lorem.txt",
@@ -43,6 +46,7 @@ def test_scan_zip(mocker):
                 "compression_size": 1425,
                 "compression_rate": 64.51,
                 "extracted": True,
+                "encrypted": False,
             },
         ],
         "compression_rate": 64.51,
@@ -75,6 +79,7 @@ def test_scan_zip_count_limit(mocker):
                 "compression_size": 1425,
                 "compression_rate": 64.51,
                 "extracted": True,
+                "encrypted": False,
             },
             {
                 "file_name": "hidden/lorem-readonly.txt",
@@ -82,6 +87,7 @@ def test_scan_zip_count_limit(mocker):
                 "compression_size": 1425,
                 "compression_rate": 64.51,
                 "extracted": True,
+                "encrypted": False,
             },
         ],
         "compression_rate": 64.51,
@@ -115,6 +121,7 @@ def test_scan_zip_metadata_limit(mocker):
                 "compression_size": 1425,
                 "compression_rate": 64.51,
                 "extracted": True,
+                "encrypted": False,
             },
             {
                 "file_name": "hidden/lorem-readonly.txt",
@@ -122,6 +129,7 @@ def test_scan_zip_metadata_limit(mocker):
                 "compression_size": 1425,
                 "compression_rate": 64.51,
                 "extracted": True,
+                "encrypted": False,
             },
             {
                 "file_name": "hidden/lorem.txt",
@@ -129,6 +137,7 @@ def test_scan_zip_metadata_limit(mocker):
                 "compression_size": 1425,
                 "compression_rate": 64.51,
                 "extracted": False,
+                "encrypted": False,
             },
             {
                 "file_name": "lorem.txt",
@@ -136,6 +145,7 @@ def test_scan_zip_metadata_limit(mocker):
                 "compression_size": 1425,
                 "compression_rate": 64.51,
                 "extracted": False,
+                "encrypted": False,
             },
         ],
         "compression_rate": 64.51,
@@ -164,35 +174,40 @@ def test_scan_zip_aes256(mocker):
     test_scan_event = {
         "elapsed": mock.ANY,
         "flags": ["encrypted"],
-        "total": {"files": 4, "extracted": 0},
+        "total": {"files": 4, "extracted": 4},
+        "password": ["password"],
         "files": [
             {
                 "file_name": "hidden/lorem-hidden.txt",
                 "file_size": 4015,
                 "compression_size": 1453,
                 "compression_rate": 63.81,
-                "extracted": False,
+                "extracted": True,
+                "encrypted": True,
             },
             {
                 "file_name": "hidden/lorem-readonly.txt",
                 "file_size": 4015,
                 "compression_size": 1453,
                 "compression_rate": 63.81,
-                "extracted": False,
+                "extracted": True,
+                "encrypted": True,
             },
             {
                 "file_name": "hidden/lorem.txt",
                 "file_size": 4015,
                 "compression_size": 1453,
                 "compression_rate": 63.81,
-                "extracted": False,
+                "extracted": True,
+                "encrypted": True,
             },
             {
                 "file_name": "lorem.txt",
                 "file_size": 4015,
                 "compression_size": 1453,
                 "compression_rate": 63.81,
-                "extracted": False,
+                "extracted": True,
+                "encrypted": True,
             },
         ],
         "compression_rate": 63.81,
@@ -202,6 +217,7 @@ def test_scan_zip_aes256(mocker):
         mocker=mocker,
         scan_class=ScanUnderTest,
         fixture_path=Path(__file__).parent / "fixtures/test_aes256_password.zip",
+        options={"crack_pws": True, "log_pws": True},
     )
 
     TestCase.maxDiff = None
@@ -225,6 +241,7 @@ def test_scan_zip_big(mocker):
                 "compression_size": 496891,
                 "compression_rate": 99.9,
                 "extracted": False,
+                "encrypted": False,
             },
         ],
         "compression_rate": 99.9,
@@ -258,6 +275,7 @@ def test_scan_zip_empty(mocker):
                 "compression_size": 0,
                 "compression_rate": 0,
                 "extracted": False,
+                "encrypted": False,
             },
             {
                 "file_name": "test.txt",
@@ -265,6 +283,7 @@ def test_scan_zip_empty(mocker):
                 "compression_size": 1449,
                 "compression_rate": 63.84,
                 "extracted": True,
+                "encrypted": False,
             },
         ],
         "compression_rate": 63.84,
@@ -278,3 +297,65 @@ def test_scan_zip_empty(mocker):
 
     TestCase.maxDiff = None
     TestCase().assertDictEqual(test_scan_event, scanner_event)
+
+
+def test_scan_zip_mixed_zipcrypto(mocker):
+    """
+    Pass: Sample event matches output of scanner.
+    Failure: Unable to load file or sample event fails to match.
+    """
+
+    test_scan_event = {
+        "elapsed": mock.ANY,
+        "flags": ["encrypted"],
+        "total": {"files": 4, "extracted": 3},
+        "password": ["password"],
+        "files": [
+            {
+                "file_name": "test.txt",
+                "file_size": 4007,
+                "compression_size": 1421,
+                "compression_rate": 64.54,
+                "extracted": True,
+                "encrypted": False,
+            },
+            {
+                "file_name": "test_aes256.txt",
+                "file_size": 4007,
+                "compression_size": 1449,
+                "compression_rate": 63.84,
+                "extracted": True,
+                "encrypted": True,
+            },
+            {
+                "file_name": "test_zipcrypto.txt",
+                "file_size": 4007,
+                "compression_size": 1433,
+                "compression_rate": 64.24,
+                "extracted": True,
+                "encrypted": True,
+            },
+            {
+                "file_name": "test_zipcrypto_badpw.txt",
+                "file_size": 4007,
+                "compression_size": 1433,
+                "compression_rate": 64.24,
+                "extracted": False,
+                "encrypted": True,
+            },
+        ],
+        "compression_rate": 64.21,
+    }
+
+    scanner_event = run_test_scan(
+        mocker=mocker,
+        scan_class=ScanUnderTest,
+        fixture_path=Path(__file__).parent / "fixtures/test_mixedcrypto.zip",
+        options={"crack_pws": True, "log_pws": True},
+    )
+
+    TestCase.maxDiff = None
+    TestCase().assertDictEqual(test_scan_event, scanner_event)
+
+
+# test_aes256_password.zip
